0d82c28b3de461e5f7c5fb302f77ca61386a42ab
k17081kk <k17081kk@pck17081kk.local>
2020-01-16 16:06:43 +0900

    ハンドラー更新

--- app/controller/ArtistHandlers.py
+++ app/controller/ArtistHandlers.py
@@ -6,7 +6,7 @@ from controller.AuthenticationHandlers import SigninBaseHandler
 
 class ArtistCreateHandler(SigninBaseHandlerX):
     def get(self):
-        if not self.:
+        if not self.current_user:
             self.redirect("/login")
             return
         # サインインユーザーの取得
@@ -18,34 +18,31 @@ class ArtistCreateHandler(SigninBaseHandlerX):
 
      def post(self):
         if not self.current_user:
-            self.redirect("/signin")
+            self.redirect("/login")
             return
         # サインインユーザーの取得
-        _id = tornado.escape.xhtml_escape(self.current_user)
+        _username = tornado.escape.xhtml_escape(self.current_user)
         _signedInUser = user.find(int(_id))
 
         # POSTされたパラメータを取得
-        p_date = self.get_argument("form-date", None)
-        p_summary = self.get_argument("form-summary", None)
-        p_detail = self.get_argument("form-detail", None)
-        p_income = self.get_argument("form-income", None)
-        p_expenses = self.get_argument("form-expenses", None)
-
-        # 現金出納帳データの組み立て
-        cb = cashbook.build()
-        cb.attr["user_id"] = int(_id) # ユーザーIDはサインインユーザーより取得
+        p_artist_name = self.get_argument("artist_name", None)
+        p_fan_class = self.get_argument("fan_class", None)
+        p_sex = self.get_argument("sex", None)
+        p_genre = self.get_argument("genre", None)
+        
+        # データベースの作成
+        ast = artist.build()
+        ast.attr["user_id"] = int(_id) # ユーザーIDはサインインユーザーより取得
 
         # パラメータエラーチェック
         errors = []
-        if p_date is None:
-            errors.append("日付は必須です。")
-        else:
-            # 文字列をdatetime.dateオブジェクトへをキャスト
-            cb.attr["date"] = datetime.datetime.strptime(p_date, '%Y-%m-%d').date()
-            # 年月計算
-            cb.attr["ym"] = cb.attr["date"].year * 100 + cb.attr["date"].month
+        if p_artist_name
+
+        if p_fan_class is None:
+            errors.append("年齢層の入力は必須です。")
+        ast.attr["fan_class"] = p_fan_class
         
-        if p_summary is None: errors.append("摘要は必須です。")
+        if p_ is None: errors.append("摘要は必須です。")
         cb.attr["summary"] = p_summary
         cb.attr["detail"] = p_detail
 
