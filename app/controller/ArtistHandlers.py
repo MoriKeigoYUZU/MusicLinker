fb75ec7677342053151d0b7804a820ef4fed0f38
AkariUC <akari@pluslab.org>
2020-01-22 13:27:14 +0900

    mypageartist 要素 日本語表示

--- app/controller/ArtistHandlers.py
+++ app/controller/ArtistHandlers.py
@@ -28,57 +28,57 @@ class UserMyPageHandler(LoginBaseHandler):
         favorite_genre = result.attr["genre"]
 
         if favorite_fan_class == "":
-            _favorite_fan_class = '選択しない'
+            favorite_fan_class = '選択しない'
         elif favorite_fan_class == "under_twenty":
-            _favorite_fan_class = '~20'
+            favorite_fan_class = '~20'
         elif favorite_fan_class == "twenties":
-            _favorite_fan_class = '20~30'
+            favorite_fan_class = '20~30'
         elif favorite_fan_class == "thirties":
-            _favorite_fan_class = '30~40'
+            favorite_fan_class = '30~40'
         elif favorite_fan_class == "forties":
-            _favorite_fan_class = '40~50'
+            favorite_fan_class = '40~50'
         elif favorite_fan_class == "fifties":
-            _favorite_fan_class = '50~60'
+            favorite_fan_class = '50~60'
         elif favorite_fan_class == "over_seventy":
-            _favorite_fan_class = '70~'
+            favorite_fan_class = '70~'
 
         if favorite_sex == "":
-            _favorite_sex = '選択しない'
+            favorite_sex = '選択しない'
         elif favorite_sex == "male":
-            _favorite_sex = '男'
+            favorite_sex = '男'
         elif favorite_sex == "female":
-            _favorite_sex = '女'
+            favorite_sex = '女'
 
         if favorite_genre == "":
-            _favorite_genre = '選択しない'
+            favorite_genre = '選択しない'
         elif favorite_genre == "pop":
-            _favorite_genre = 'POP'
+            favorite_genre = 'POP'
         elif favorite_genre == "dance":
-            _favorite_genre = 'ダンス'
+            favorite_genre = 'ダンス'
         elif favorite_genre == "animation":
-            _favorite_genre = 'アニメ'
+            favorite_genre = 'アニメ'
         elif favorite_genre == "jazz":
-            _favorite_genre = 'ジャズ'
+            favorite_genre = 'ジャズ'
         elif favorite_genre == "reggae":
-            _favorite_genre = 'レゲエ'
+            favorite_genre = 'レゲエ'
         elif favorite_genre == "rbsoul":
-            _favorite_genre = 'R&Bソウル'
+            favorite_genre = 'R&Bソウル'
         elif favorite_genre == "classicalMusic":
-            _favorite_genre = 'クラシック'
+            favorite_genre = 'クラシック'
         elif favorite_genre == "electronicMusic":
-            _favorite_genre = 'エレクトロニック'
+            favorite_genre = 'エレクトロニック'
         elif favorite_genre == "rock":
-            _favorite_genre = 'ロック'
+            favorite_genre = 'ロック'
         elif favorite_genre == "world":
-            _favorite_genre = 'ワールド'
+            favorite_genre = 'ワールド'
         elif favorite_genre == "alternative":
-            _favorite_genre = 'オルタナティブ'
+            favorite_genre = 'オルタナティブ'
         elif favorite_genre == "popularSong":
-            _favorite_genre = '歌謡曲'
+            favorite_genre = '歌謡曲'
         elif favorite_genre == "blues":
-            _favorite_genre = 'ブルース'
+            favorite_genre = 'ブルース'
         elif favorite_genre == "hiphopRap":
-            _favorite_genre = 'ヒップホップ＆ラップ'
+            favorite_genre = 'ヒップホップ＆ラップ'
 
         if favorite_id == 0:
             self.render("mypageUser.html", user=result,
@@ -91,9 +91,9 @@ class UserMyPageHandler(LoginBaseHandler):
             favorite_artist = user.find(favorite_id)
             self.render("mypageUser.html", user=result,
                         favorite_artist_name=favorite_artist.attr["artist_name"],
-                        favorite_fan_class=_favorite_fan_class,
-                        favorite_sex=_favorite_sex,
-                        favorite_genre=_favorite_genre,
+                        favorite_fan_class=favorite_fan_class,
+                        favorite_sex=favorite_sex,
+                        favorite_genre=favorite_genre,
                         messages=messages, errors=[])
 
 
@@ -114,7 +114,65 @@ class ArtistMyPageHandler(LoginBaseHandler):
 
         # データ取得
         result = user.find(int(_id))
+        _fan_class = result.attr["fan_class"]
+        _sex = result.attr["sex"]
+        _genre = result.attr["genre"]
 
-        _fan_class = result.attr
+        if _fan_class == "":
+            _fan_class = '選択していない'
+        elif _fan_class == "under_twenty":
+            _fan_class = '~20'
+        elif _fan_class == "twenties":
+            _fan_class = '20~30'
+        elif _fan_class == "thirties":
+            _fan_class = '30~40'
+        elif _fan_class == "forties":
+            _fan_class = '40~50'
+        elif _fan_class == "fifties":
+            _fan_class = '50~60'
+        elif _fan_class == "over_seventy":
+            _fan_class = '70~'
+
+        if _sex == "":
+            _sex = '選択していない'
+        elif _sex == "male":
+            _sex = '男'
+        elif _sex == "female":
+            _sex = '女'
+
+        if _genre == "":
+            _genre = '選択していない'
+        elif _genre == "pop":
+            _genre = 'POP'
+        elif _genre == "dance":
+            _genre = 'ダンス'
+        elif _genre == "animation":
+            _genre = 'アニメ'
+        elif _genre == "jazz":
+            _genre = 'ジャズ'
+        elif _genre == "reggae":
+            _genre = 'レゲエ'
+        elif _genre == "rbsoul":
+            _genre = 'R&Bソウル'
+        elif _genre == "classicalMusic":
+            _genre = 'クラシック'
+        elif _genre == "electronicMusic":
+            _genre = 'エレクトロニック'
+        elif _genre == "rock":
+            _genre = 'ロック'
+        elif _genre == "world":
+            _genre = 'ワールド'
+        elif _genre == "alternative":
+            _genre = 'オルタナティブ'
+        elif _genre == "popularSong":
+            _genre = '歌謡曲'
+        elif _genre == "blues":
+            _genre = 'ブルース'
+        elif _genre == "hiphopRap":
+            _genre = 'ヒップホップ＆ラップ'
+
         self.render("mypageArtist.html", user=result,
+                    fan_class=_fan_class,
+                    sex=_sex,
+                    genre=_genre,
                     messages=messages, errors=[])
